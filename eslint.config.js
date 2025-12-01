dc87fad0b25ef311d1e557aec81f6a8944d30401
Vladislav Ezerski <vladezerski@gmail.com>
2025-12-01 15:51:02 +0100

    chore: setup prettier and update eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config")
+const expoConfig = require("eslint-config-expo/flat")
+const eslintPluginPrettier = require("eslint-plugin-prettier/recommended")
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettier,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*", "ios/*", "android/*"],
+    rules: {
+      "prettier/prettier": ["error"],
+    },
   },
-]);
+])
